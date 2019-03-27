b8edb01ea764add638f45e6ea6170fb5463261a8
Tegan Gordon <tegordon@e1z2r8p27.42.us.org>
2019-03-27 07:24:21 -0700

    restrict removed

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: tegordon <tegordon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/26 09:24:00 by tegordon          #+#    #+#             */
-/*   Updated: 2019/03/27 02:47:44 by tegordon         ###   ########.fr       */
+/*   Updated: 2019/03/27 07:20:40 by tegordon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 
 #include "libft.h"
 
-char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n)
+char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
 	size_t i;
 	size_t j;
